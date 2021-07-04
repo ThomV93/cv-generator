823e97e1246e4b6d469f1e5ec0e9080de31a478d
Thomas Veit <thomas.veit93@gmail.com>
2021-07-04 17:52:08 +0100

    mockup button

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,19 +11,26 @@ const Header = props => {
   return (
     <header>
       <h1>CV Generator</h1>
-      <button
-        className="reset-print-btn"
-        type="button"
-        onClick={display ? () => {resetState()} : () => {handlePrint()}}
-        style={{backgroundColor: display ? "#ff2f2f" : "#4CAF50"}}>
-          {display ? "Reset All" : "Print PDF"}
-      </button>
-      <button 
-        className="preview-form-btn"
-        type="button" 
-        onClick={() => {togglePreview()} }>
-          {display ? "Preview" : "Form"}
-      </button>
+      <div className="btn-container">
+        <button
+          className="mockup-btn"
+          type="button">
+            Mockup
+        </button>
+        <button
+          className="reset-print-btn"
+          type="button"
+          onClick={display ? () => {resetState()} : () => {handlePrint()}}
+          style={{backgroundColor: display ? "#ff2f2f" : "#4CAF50"}}>
+            {display ? "Reset All" : "Print PDF"}
+        </button>
+        <button 
+          className="preview-form-btn"
+          type="button" 
+          onClick={() => {togglePreview()} }>
+            {display ? "Preview" : "Form"}
+        </button>
+      </div>
     </header>
   );
 };
